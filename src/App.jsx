2f2e18d27be1d9d104a4f0c1d01dbccc0a470e21
Rohan Nayan <rohannayan405@gmal.com>
2023-09-23 15:54:36 +0530

    Import Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 function App() {
   return (
-    <div>
-      3D Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="realtive z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
