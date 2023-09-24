13451852253dd87a7872863bab5fabdb67c1530a
Rohan Nayan <rohannayan405@gmal.com>
2023-09-24 13:30:11 +0530

    About Section Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
       <motion.p variants={fadeIn("", "", 0.1, 1)} 
                 className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Hello, my name is Rohan Nayan and I am currently a third year computer science student at VIT Vellore. I am passionate about technology and have been actively learning programming languages such as C++, Python, and Java. I have experience working with web development technologies including HTML, CSS, JavaScript, Node.js, and MongoDB. I have completed a few projects using these technologies to build web applications.
+        I am currently a third year computer science student at VIT Vellore. I am passionate about technology and have been actively learning programming languages such as C++, Python, and Java. I have experience working with web development technologies including  JavaScript, Node.js, Express.js, React.js and MongoDB. I have completed a few projects using these technologies to build web applications.
         I am excited to continue learning and developing my skills in computer science and hope to one day contribute to the industry. I am actively seeking opportunities to gain experience and expand my knowledge.
       </motion.p>
 
