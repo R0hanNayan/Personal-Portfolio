1bc14b50b156b980854081889978ab4be0feae0b
Rohan Nayan <rohannayan405@gmail.com>
2023-09-25 17:55:39 +0530

    Added Tags in project cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -39,7 +39,16 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link}) 
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
           <p className='mt-2 text-secondary text-[14px]'>{description}</p>
         </div>
-        
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </Tilt>
     </motion.div>
   )
