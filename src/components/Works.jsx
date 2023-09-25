12d9f0c36765a5f1201d6dfe32359df2ae8143d3
Rohan Nayan <rohannayan405@gmail.com>
2023-09-25 17:49:42 +0530

    Added Card and Github Links

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,36 @@ import { fadeIn, textVariant } from '../utils/motion';
 const ProjectCard = ({index, name, description, tags, image, source_code_link}) =>{
   return(
     <motion.div variants={fadeIn("up","spring", index*0.5, 0.75)}>
-      test
+      <Tilt
+        option={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img 
+            src={image}
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
+        </div>
+
+        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div 
+            onClick={()=> window.open(source_code_link, "_blank")}
+            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+          >
+            <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
+          </div>
+        </div>
+
+        <div>
+          
+        </div>
+
+      </Tilt>
     </motion.div>
   )
 }
