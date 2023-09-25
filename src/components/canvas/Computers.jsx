48aaf0fb5c79167ad7231a1ae96189ba6f6ca0b9
Rohan Nayan <rohannayan405@gmail.com>
2023-09-25 23:08:27 +0530

    Computer Canvas Changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,9 +12,9 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
-        angle={1}
-        penumbra={1.1}
-        intensity={20000}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
@@ -22,8 +22,8 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -2, -2.2] : [0, -3.2, -1.5]}
-        rotation={[0.009, -0.2, -0.1]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
