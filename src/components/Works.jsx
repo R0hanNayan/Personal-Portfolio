30c7aa5aef1bd24370007b118a07996d12d83976
Rohan Nayan <rohannayan405@gmail.com>
2023-09-25 17:41:50 +0530

    ProjectCard Setup

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,20 +7,20 @@ import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
-const ProjectCard = () =>{
-  
+const ProjectCard = ({index, name, description, tags, image, source_code_link}) =>{
+  return(
+    <motion.div variants={fadeIn("up","spring", index*0.5, 0.75)}>
+      test
+    </motion.div>
+  )
 }
 
 const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          My work
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Projects.
-        </h2>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
@@ -31,16 +31,14 @@ const Works = () => {
           Welcome to my Projects Section, a snapshot of my journey where I've brought ideas to life, solved challenges, and made an impact. These projects reflect my passion for creativity and problem-solving. Dive in and witness the results of my dedication and expertise.
         </motion.p>
       </div>
+
       <div className='mt-20 flex flex-wrap gap-7'>
-        {projects.map((project, index) => {
-          <ProjectCard 
-            key={`project-${index}`}
-            index={index}
-            {...project}
-          />
-        })}
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
       </div>
     </>
+
   )
 }
 
