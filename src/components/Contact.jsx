0fd739ce4271d3dcb24329dd95ccb775000cdd39
Rohan Nayan <rohannayan405@gmail.com>
2023-09-25 20:16:09 +0530

    Contact Component Setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useState, useRef} from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
   return (
