6e2cf8a22f59960747d14658e37d00223113acda
Rohan Nayan <rohannayan405@gmail.com>
2023-09-26 11:50:39 +0530

    Skill Component Changes

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -6,48 +6,47 @@ import {
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
-
   const [decal] = useTexture([props.imgUrl]);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25}/>
+      <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} />
-        <meshStandardMaterial 
-          color={'#fff8eb'}
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-        <Decal 
+        <Decal
           position={[0, 0, 1]}
-          rotation={[2*Math.PI, 0, 6.25]}
-          flatShading
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
           map={decal}
+          flatShading
         />
       </mesh>
     </Float>
-  )
-}
+  );
+};
 
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
       frameloop='demand'
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-        />
-        <Ball imgUrl={icon}/>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default BallCanvas;
