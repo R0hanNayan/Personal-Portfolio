34a31133328ed6a0ecea66d0f209d9bb6b2cbea3
Rohan Nayan <rohannayan405@gmail.com>
2023-09-26 12:00:32 +0530

    ComputerCanvas Changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,9 +12,9 @@ const Computers = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
-        angle={1}
+        angle={0.12}
         penumbra={1}
-        intensity={20000}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
